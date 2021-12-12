2f33113dbeb31deb73f250a1990bfd5f38eed111
Antonio Galvão <galvontony@gmail.com>
2021-12-12 18:22:06 -0300

    Add mocks

--- src/components/AssetList/index.tsx
+++ src/components/AssetList/index.tsx
@@ -5,55 +5,9 @@ import { AssetCard } from '../AssetCard';
 
 import { styles } from './styles';
 
-export function AssetList() {
-  const assets = [
-    {
-      id: '1',
-      code: 'BTC',
-      name: 'Bitcoin',
-      icon: 'bitcoin.png',
-      amount: '0.00012',
-      value: '26.46',
-      increase: '15.3',
-    },
-    {
-      id: '2',
-      code: 'ETH',
-      name: 'Etherium',
-      icon: 'bitcoin.png',
-      amount: '0.00012',
-      value: '37.30',
-      increase: '-15.3',
-    },
-    {
-      id: '3',
-      code: 'LTC',
-      name: 'Litecoin',
-      icon: 'bitcoin.png',
-      amount: '0.00012',
-      value: '89.40',
-      increase: '-1.1',
-    },
-    {
-      id: '4',
-      code: 'SOL',
-      name: 'Solana',
-      icon: 'bitcoin.png',
-      amount: '0.00012',
-      value: '142.50',
-      increase: '15.3',
-    },
-    {
-      id: '5',
-      code: 'XRP',
-      name: 'Ripple',
-      icon: 'bitcoin.png',
-      amount: '0.00012',
-      value: '895.40',
-      increase: '15.3',
-    },
-  ];
+import { assets } from '../../mocks/assets';
 
+export function AssetList() {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {assets.map((asset) => (
--- src/components/CurrencyList/index.tsx
+++ src/components/CurrencyList/index.tsx
@@ -5,47 +5,11 @@ import { CurrencyCard } from '../CurrencyCard';
 
 import { styles } from './styles';
 
+import { currencies } from '../../mocks/currencies';
+
 type Props = ScrollViewProps;
 
 export function CurrencyList({ ...rest }: Props) {
-  const currencies = [
-    {
-      id: '1',
-      code: 'BTC',
-      name: 'Bitcoin',
-      unitValue: '$54,382.64',
-      increase: '15.3',
-    },
-    {
-      id: '2',
-      code: 'ETH',
-      name: 'Etherium',
-      unitValue: '$4,145.61',
-      increase: '-2.1',
-    },
-    {
-      id: '3',
-      code: 'LTC',
-      name: 'Litecoin',
-      unitValue: '$207.3',
-      increase: '-1.1',
-    },
-    {
-      id: '4',
-      code: 'SOL',
-      name: 'Solana',
-      unitValue: '$227.93',
-      increase: '15.3',
-    },
-    {
-      id: '5',
-      code: 'XRP',
-      name: 'Ripple',
-      unitValue: '$1.0358',
-      increase: '15.3',
-    },
-  ];
-
   return (
     <ScrollView
       style={styles.container}

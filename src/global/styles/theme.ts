ca2adf11b6d6a103444b5840b097a4a3be161ffe
Antonio Galvão <galvontony@gmail.com>
2021-12-12 01:25:44 -0300

    Add styles theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    primary: '#5EDE99',
+    secondary: '#F288F4',
+    success: '#00CB6A',
+    warning: '#EFBE24',
+    danger: '#FE6666',
+    dark: '#04150C',
+    light: '#F7F7F7',
+
+    // TEXT
+    black: '#152C07',
+    gray: '#5D5C5D',
+    gray2: '#BEBEBE',
+    white: '#FFFFFF',
+  },
+
+  fonts: {
+    title700: 'Mulish_700Bold',
+    title600: 'Mulish_600SemiBold',
+    text500: 'Montserrat_500Medium',
+    text400: 'Mulish_400Regular',
+  },
+};

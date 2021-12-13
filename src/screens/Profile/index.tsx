842b426224062d681af505f8b74c655e8e015f46
Antonio Galvão <galvontony@gmail.com>
2021-12-13 04:28:07 -0300

    Improve Profile content

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
@@ -48,44 +47,55 @@ export function Profile() {
         </View>
       </View>
 
-      <View style={styles.settingsContainer}>
-        <Text style={styles.settingsHeader}>Settings</Text>
-
+      <ScrollView showsVerticalScrollIndicator={false}>
         <SettingCard
-          title='Privacy'
-          description='Change email and password'
-          iconBackgroundColor={theme.colors.lightBlue}
+          title='Transactions'
+          description='Your transactions list'
+          iconBackgroundColor={theme.colors.lightGreen}
         >
-          <Fontisto name='locked' size={20} color={theme.colors.blue} />
+          <Ionicons name='newspaper' size={20} color={theme.colors.primary} />
         </SettingCard>
-        <View style={{ marginBottom: 8 }} />
+        <View style={{ marginBottom: 24 }} />
 
-        <SettingCard
-          title='Payment'
-          description='Update payment settings'
-          iconBackgroundColor={theme.colors.lightPurple}
-        >
-          <Fontisto name='locked' size={20} color={theme.colors.purple} />
-        </SettingCard>
-        <View style={{ marginBottom: 8 }} />
+        <View style={styles.settingsContainer}>
+          <Text style={styles.settingsHeader}>Settings</Text>
 
-        <SettingCard
-          title='Notifications'
-          description='Change notification settings'
-          iconBackgroundColor={theme.colors.lightWarning}
-        >
-          <Fontisto name='locked' size={20} color={theme.colors.warning} />
-        </SettingCard>
-        <View style={{ marginBottom: 16 }} />
+          <SettingCard
+            title='Privacy'
+            description='Change email and password'
+            iconBackgroundColor={theme.colors.lightBlue}
+          >
+            <Fontisto name='locked' size={20} color={theme.colors.blue} />
+          </SettingCard>
+          <View style={{ marginBottom: 8 }} />
 
-        <SettingCard
-          title='Logout'
-          description='Exit the app'
-          iconBackgroundColor={theme.colors.lightDanger}
-        >
-          <Fontisto name='locked' size={20} color={theme.colors.danger} />
-        </SettingCard>
-      </View>
+          <SettingCard
+            title='Payment'
+            description='Update payment settings'
+            iconBackgroundColor={theme.colors.lightPurple}
+          >
+            <Ionicons name='card' size={20} color={theme.colors.purple} />
+          </SettingCard>
+          <View style={{ marginBottom: 8 }} />
+
+          <SettingCard
+            title='Notifications'
+            description='Change notification settings'
+            iconBackgroundColor={theme.colors.lightWarning}
+          >
+            <Fontisto name='bell-alt' size={20} color={theme.colors.warning} />
+          </SettingCard>
+          <View style={{ marginBottom: 16 }} />
+
+          <SettingCard
+            title='Logout'
+            description='Exit the app'
+            iconBackgroundColor={theme.colors.lightDanger}
+          >
+            <Ionicons name='exit' size={20} color={theme.colors.danger} />
+          </SettingCard>
+        </View>
+      </ScrollView>
     </View>
   );
 }

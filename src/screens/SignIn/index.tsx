481abf7c6e1f855c9c95f436372f0db15691a067
Antonio Galvão <galvontony@gmail.com>
2021-12-30 00:29:52 -0300

    Add basic signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import { Image, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Image, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { RectButton } from 'react-native-gesture-handler';
 
-import LogoImg from '../../assets/logo.png';
+import { api } from '../../services/api';
+
 import { Button } from '../../components/Button';
 import { LabeledTextInput } from '../../components/LabeledTextInput';
+import LogoImg from '../../assets/logo.png';
 
 import { styles } from './styles';
-import { theme } from '../../global/styles/theme';
-
-import { Feather } from '@expo/vector-icons';
 
 export function SignIn() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function handleLogin() {
-    navigation.navigate('Root');
+  async function handleLogin() {
+    try {
+      await api.post('auth/session', { email, password });
+      navigation.navigate('Root');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Invalid credentials!');
+    }
   }
 
   function handleNavigateToRegisterScreen() {
@@ -36,9 +43,19 @@ export function SignIn() {
         </View>
 
         <View style={styles.form}>
-          <LabeledTextInput title='Email' />
+          <LabeledTextInput
+            title='Email'
+            keyboardType='email-address'
+            onChangeText={setEmail}
+            value={email}
+          />
 
-          <LabeledTextInput title='Password' />
+          <LabeledTextInput
+            title='Password'
+            secureTextEntry={true}
+            onChangeText={setPassword}
+            value={password}
+          />
 
           <Text style={styles.forgotPassword}>Forgot your password?</Text>
 
